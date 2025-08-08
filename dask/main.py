dd72a41b3ad04466f8dcb8cf3a2e21953cd571b8
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-08-08 20:45:11 +0300

    Using dask array and dask dataframe

--- dask/main.py
+++ dask/main.py
@@ -1,1 +1,16 @@
-import dask
+import dask.array as da
+import dask.dataframe as dd
+import numpy as np
+
+arr = np.arange(1000)
+d_arr = da.from_array(arr)
+d_arr2 = da.from_array(arr, chunks=(100,)) # type: ignore
+
+print(f"The numpy array 10: {arr[:10]}")
+print(f"The Dask array complete: {d_arr}")
+print(f"The Dask array complete: {d_arr2}")
+
+df = dd.read_csv("train.csv")
+d_arr3 = df.to_dask_array()
+# print(f"The Complete DataFrame is: {df}");
+print(f"The Dask Array is: {d_arr3}");
